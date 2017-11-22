ab7e8dbf290b9ab6d232fb087547849a48039aee
cwfroom <cwfroom123@gmail.com>
2017-11-21 16:48:40 -0800

    Buildings on side

---
+++ Pixel_Invaders/Assets/_Scripts/Managers/BuildingMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMover : MonoBehaviour {
+    public bool moving = true;
+    public float speed = 5.0f;
+    public float borderY = -16.0f;
+    public float resetY = 14.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving)
+        {
+            transform.position = transform.position - new Vector3(0, speed, 0) * Time.deltaTime;
+            if (transform.position.y < borderY)
+            {
+                transform.position = new Vector3(0, resetY, 0);
+            }
+        }
+	}
+}
